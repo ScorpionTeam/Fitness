b8658c7dd78aa841336988baa9e6f36f0e9e0431
Jarven <769558579@qq.com>
2017-07-23 11:50:32 +0800

    团课

---
+++ src/main/java/com/fitness/api/controller/GroupClassController.java
@@ -0,0 +1,13 @@
+package com.fitness.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 团课
+ * Created on 2017/7/23.
+ */
+@RestController
+@RequestMapping("groupClass")
+public class GroupClassController {
+}
