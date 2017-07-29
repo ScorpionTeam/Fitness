68b96ea31b76f59d480908b1bc1d80ccf1719e05
Jarven <769558579@qq.com>
2017-07-29 13:20:08 +0800

    教练印象

--- src/main/java/com/fitness/api/service/ImpressionService.java
+++ src/main/java/com/fitness/api/service/ImpressionService.java
@@ -1,8 +1,27 @@
 package com.fitness.api.service;
 
+import com.fitness.api.domain.Impression;
+import com.fitness.result.BaseResult;
+
+import java.util.List;
+
 /**
  * 印象业务逻辑接口
  * Created on 2017/7/29.
  */
 public interface ImpressionService {
+
+    /**
+     * 创建印象
+     * @param impression
+     * @return
+     */
+    BaseResult add(Impression impression);
+
+    /**
+     * 根据教练id查询印象列表
+     * @param coachId
+     * @return
+     */
+    List<Impression> list(Long coachId);
 }
