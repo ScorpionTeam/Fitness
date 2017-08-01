44f41674f54101aa6837f79a4a51ed7f4a7252c9
Jarven <769558579@qq.com>
2017-08-01 20:37:21 +0800

    水吧SQL

--- src/main/java/com/fitness/api/impl/WaterServiceImpl.java
+++ src/main/java/com/fitness/api/impl/WaterServiceImpl.java
@@ -6,50 +6,66 @@ import com.fitness.api.service.WaterService;
 import com.fitness.result.BaseResult;
 import com.fitness.result.page.PageResult;
 import com.fitness.result.page.PageService;
+import com.mysql.jdbc.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created on 2017/8/1.
  */
 @Service
-public class WaterServiceImpl implements WaterService,PageService {
+public class WaterServiceImpl implements WaterService, PageService {
 
     @Autowired
     private WaterDao waterDao;
 
     /**
      * 创建
+     *
      * @param water
      * @return
      */
     @Override
     public BaseResult add(Water water) {
-        return null;
+        Integer result = waterDao.add(water);
+        if (result > 0)
+            return BaseResult.success("新增成功");
+        return BaseResult.error("ADD_FAIL", "新增失败");
     }
 
     /**
      * 删除
+     *
      * @param id
      * @return
      */
     @Override
     public BaseResult del(Long id) {
-        return null;
+        Integer result = waterDao.del(id);
+        if (result > 0)
+            return BaseResult.success("删除成功");
+        return BaseResult.error("DELETE_FAIL", "删除失败");
     }
 
     /**
      * 修改
+     *
      * @param water
      * @return
      */
     @Override
     public BaseResult update(Water water) {
-        return null;
+        Integer result = waterDao.update(water);
+        if (result > 0)
+            return BaseResult.success("修改成功");
+        return BaseResult.error("UPDATE_FAIL", "修改失败");
     }
 
     /**
      * 列表  分页 模糊查询
+     *
      * @param pageNo
      * @param pageSize
      * @param key
@@ -57,6 +73,12 @@ public class WaterServiceImpl implements WaterService,PageService {
      */
     @Override
     public PageResult list(Integer pageNo, Integer pageSize, String key) {
-        return null;
+        if (!StringUtils.isNullOrEmpty(key))
+            key = "%" + key + "%";
+        Integer count = waterDao.count(key);
+        if (count <= 0)
+            return new PageResult(null, 0);
+        List<Water> list = waterDao.list(rowBounds(pageNo, pageSize), key);
+        return new PageResult(list, count, pageNo, pageSize);
     }
 }
