8063f96b5ed2ff647475b0d17678c2bfc4dacbbb
Jarven <769558579@qq.com>
2017-08-14 21:33:38 +0800

    后台删除 团课 -前台还存在 bug

---
+++ src/main/java/com/fitness/api/controller/MessageController.java
@@ -0,0 +1,14 @@
+package com.fitness.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * 消息模块
+ * Created on 2017/8/14.
+ */
+@RestController
+@RequestMapping("message")
+public class MessageController {
+}
